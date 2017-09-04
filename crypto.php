ba287ca7e765a29ed438b79d9963e008d44f3f3b
Frida Melin <fm222gn@student.lnu.se>
2017-09-04 13:24:26 +0200

    Encrypt-delen funkar nu

--- crypto.php
+++ crypto.php
@@ -1,11 +1,28 @@
 <?php
 
 class Crypto {
+    public function enCrypt($input){
+        $consonants = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+		if(strlen($input) > 0) {
+			$output = "";
+			//loopar igenom input
+			for($i = 0; $i < strlen($input); $i++) {
+				
+			//Om det finns en konsonant, lägg till o emellan bokstäverna. 
+			//annars skriv ut bokstaven 	
+				if(in_array($input[$i], $consonants)) {
+					$output = $output . $input[$i] . "o" . $input[$i];
+				}
+				else {
+					$output= $output . $input[$i];
+				}
+			}
+			
+            return $output;
+		}
+    }
+
     public function deCrypt($input){
         return "hej";
     }
-   public function enCrypt($input){
-
-    }
-
 }
--- index.php
+++ index.php
@@ -7,44 +7,43 @@ require_once('crypto.php');
 
 $crypto = new Crypto();
 
-echo $crypto->deCrypt("d");
-
 	$INPUT = "";
+	$encrypted = "";
+	$decrypted = "";
 	
-	function Crypto($input) {
-		$consonants = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
-		var_dump($input);
-		if(strlen($input) > 0) {
-			$output = "";
-			//loopar igenom input
-			for($i = 0; $i < strlen($input); $i++) {
-				
-			//Om det finns en konsonant, lägg till o emellan bokstäverna. 
-			//annars skriv ut bokstaven 	
-				if(in_array($input[$i], $consonants)) {
-					$output = $output . $input[$i] . "o" . $input[$i];
-				}
-				else {
-					$output= $output . $input[$i];
-				}
-			}
-			echo $output;
-		}
-	}
-
-	if(isset($_POST['test']))
+	if(isset($_POST['encrypt']))
 	{   
-    	$text = $_POST['test'];
-		$INPUT = $text;
-		Crypto($INPUT);
+    	$encrypt = $_POST['encrypt'];
+		$encrypted = $crypto->enCrypt($encrypt);
 	}
 
-	
+	if(isset($_POST['decrypt']))
+	{   
+    	$decrypt = $_POST['decrypt'];
+		$decrypted = $crypto->deCrypt($decrypt);
+	}
 ?>
 
 <form method='post' action='index.php'>
-	<textarea id="testarea" name="test" rows=10></textarea>
-	<input type='submit' name="submit"/>
+	<p>Encrypt</p>
+	<textarea name="encrypt" rows=10>
+	<?php 
+			echo $decrypted;
+		?>
+	</textarea>
+	<br>
+	<input type='submit' value="Encrypt"/>
+</form>
+
+<form method='post' action='index.php'>
+	<p>Decrypt</p>	
+	<textarea name="decrypt" rows=10>
+		<?php 
+			echo $encrypted;
+		?>
+	</textarea>
+	<br>
+	<input type='submit' value="Decrypt"/>
 </form>
 
 </body>
