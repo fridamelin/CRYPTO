47fa93597b15faee46791823ca440b4acb2e65fa
Frida Melin <fm222gn@student.lnu.se>
2017-09-04 13:41:34 +0200

    Decryt funkar, allt funkar!

--- crypto.php
+++ crypto.php
@@ -1,28 +1,41 @@
 <?php
-
 class Crypto {
+    public $consonants = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+    
     public function enCrypt($input){
-        $consonants = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
 		if(strlen($input) > 0) {
 			$output = "";
-			//loopar igenom input
+			// Loopar igenom input
 			for($i = 0; $i < strlen($input); $i++) {
-				
-			//Om det finns en konsonant, lägg till o emellan bokstäverna. 
-			//annars skriv ut bokstaven 	
-				if(in_array($input[$i], $consonants)) {
-					$output = $output . $input[$i] . "o" . $input[$i];
-				}
-				else {
-					$output= $output . $input[$i];
-				}
+                // Om det finns en konsonant, lägg till o och bokstaven igen. 
+                // Annars skriv ut vokalen	
+                if(in_array($input[$i], $this->consonants)) {
+                    $output = $output . $input[$i] . "o" . $input[$i];
+                }
+                else {
+                    $output = $output . $input[$i];
+                }
 			}
-			
             return $output;
 		}
     }
 
     public function deCrypt($input){
-        return "hej";
+		if(strlen($input) > 0) {
+			$output = "";
+			// Loopar igenom input
+			for($i = 0; $i < strlen($input); $i++) {
+                // Om det är en konsonant, så spara den till output. Därefter hoppa två steg i index
+                // Annars spara vokalen.
+				if(in_array($input[$i], $this->consonants)) {
+					$output = $output . $input[$i];
+                    $i = $i + 2;
+				}
+				else {
+					$output = $output . $input[$i];
+				}
+			}
+        }
+        return $output;
     }
 }
--- index.php
+++ index.php
@@ -2,46 +2,35 @@
 <body>
 
 <?php
-
-require_once('crypto.php');
-
-$crypto = new Crypto();
-
+	require_once('crypto.php');
+	$crypto = new Crypto();
 	$INPUT = "";
 	$encrypted = "";
 	$decrypted = "";
 	
+	// Encryptknappen
 	if(isset($_POST['encrypt']))
 	{   
-    	$encrypt = $_POST['encrypt'];
-		$encrypted = $crypto->enCrypt($encrypt);
+		$encrypted = $crypto->enCrypt($_POST['encrypt']);
 	}
 
+	// Decryptknappen
 	if(isset($_POST['decrypt']))
 	{   
-    	$decrypt = $_POST['decrypt'];
-		$decrypted = $crypto->deCrypt($decrypt);
+		$decrypted = $crypto->deCrypt($_POST['decrypt']);
 	}
 ?>
 
 <form method='post' action='index.php'>
 	<p>Encrypt</p>
-	<textarea name="encrypt" rows=10>
-	<?php 
-			echo $decrypted;
-		?>
-	</textarea>
+	<textarea name="encrypt" rows=10><?php echo $decrypted;?></textarea>
 	<br>
 	<input type='submit' value="Encrypt"/>
 </form>
 
 <form method='post' action='index.php'>
 	<p>Decrypt</p>	
-	<textarea name="decrypt" rows=10>
-		<?php 
-			echo $encrypted;
-		?>
-	</textarea>
+	<textarea name="decrypt" rows=10><?php echo $encrypted;?></textarea>
 	<br>
 	<input type='submit' value="Decrypt"/>
 </form>
